0ef28a76401138f7dbb206bdaa7c4f746c75bc75
Daniel Tabion <dctabion@gmail.com>
2018-08-06 23:16:32 -0500

    added add registrant test code

---
+++ models/registrants.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+
+// Sub Schemas
+var guestSchema = new mongoose.Schema({
+  firstName: String;
+  lastName: String;
+});
+
+// Parent Schema
+var registrantSchema = new mongoose.Scheme({
+  firstName: String,
+  lastName: String,
+  address: String,
+  city: String,
+  state: String,
+  zip: String,
+  guests: [ guestSchema ];
+});
+
+mongoose.model("Registrant", registrantSchema);
+// mongoose.model("Guest", languageSchema);
--- routes/banquet0818.js
+++ routes/banquet0818.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
+var mongoose = require('mongoose');
+var Registrant = mongoose.model('Registrant')
+
 /* GET home page. */
 router.get('/register', function(req, res, next) {
-  res.render('register', { title: 'Register' });
+  var registrant = {};
+  registrant.firstName = "Dood";
+  registrant.lastName = "McGee";
+  registrant.address = "123 Sesame St.";
+  registrant.city = "New York";
+  registrant.state = "Indiana";
+  registrant.guests = [
+    { firstname: "Travis", lastName: "McGee"},
+    { firstname: "Mike", lastName: "McGee"}
+  ];
+
+
+  Registrant.create(registrant,
+      function(err, registrant) {
+        if (err) {
+          console.log('error creating registrant! err: ', err);
+          sendJsonResponse(res, 400, err);
+        }
+        else {
+          console.log('created new registrant!');
+          sendJsonResponse(res, 200, volunteer);
+        }
+      }
+    );
 });
 
 module.exports = router;
