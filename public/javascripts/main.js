fc50b42509d581ad9e2eb39c3889adb3b6c43240
Daniel Tabion <dctabion@gmail.com>
2018-08-07 22:00:01 -0500

    fixed copy field script bug on other views.  Move scripts back to layout view

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,7 @@
 $( document ).ready(function() {
+  if (document.title == 'Banquet Registration') {
+    console.log('activating copyfield script');
+
     demo123CF_CopyFieldValue.setFieldOriginID('#registrantFirstName');
     demo123CF_CopyFieldValue.setFieldDestinationID('#guestFirstName01');
     demo123CF_CopyFieldValue.init();
@@ -6,6 +9,10 @@ $( document ).ready(function() {
     demo123CF_CopyFieldValue2.setFieldOriginID('#registrantLastName');
     demo123CF_CopyFieldValue2.setFieldDestinationID('#guestLastName01');
     demo123CF_CopyFieldValue2.init();
+  }
 
-    
+
+  else {
+    console.log('copy field script not activated');
+  }
 });
